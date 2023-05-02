7815c53d9dde0744654f5a25fdb8e3e80b5071b0
Yashas  V <yashas.v@outlook.com>
2023-05-02 19:33:45 +0530

    Update start-tomcat.ps1

--- start-tomcat.ps1
+++ start-tomcat.ps1
@@ -1,5 +1,6 @@
-# Set the environment variables for Tomcat
+# Set the environment variables for Tomcat and Java
 $env:CATALINA_HOME = "C:\tomcat"
+$env:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-11.0.19.7-hotspot\"
 
 
 # Start Tomcat on the default port (8080)
