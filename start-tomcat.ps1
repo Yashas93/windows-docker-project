016d0e5753bba9d51777d6b871b79a0f008706c8
Yashas V <yashas.v@outlook.com>
2023-05-09 06:55:36 +0000

    added Readme file

--- start-tomcat.ps1
+++ start-tomcat.ps1
@@ -2,6 +2,9 @@
 $env:CATALINA_HOME = "C:\tomcat"
 $env:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-11.0.19.7-hotspot\"
 
+#setting JDK 
+$env:JAVA_HOME = "C:\Program Files\Eclipse Adoptium\jdk-11.0.19.7-hotspot\"
+
 
 # Start Tomcat on the default port (8080)
 & "$env:CATALINA_HOME\bin\catalina.bat" run
